1177ad577b13b471ebdb6646833015bb6651d0ff
Richard Clark <richard@rvvup.com>
2024-02-27 12:01:17 +0000

    initial commit

---
+++ README.md
@@ -0,0 +1,40 @@
+# AWS Assume Role
+
+A simple utility to assume an AWS role. Statically linked, no dependencies.
+
+Uses the default AWS credential resolution mechanisms, so should support anything supported by profiles (including SSO,
+MFA etc) as well as environment variables and instance roles.
+
+# Usage
+
+Use a profile:
+
+```sh
+export $(AWS_PROFILE='my-profile' aws-assume-role -role-arn arn:aws:iam::123456789:role/role/the-role)
+```
+
+Use environment variables:
+
+```sh
+export AWS_ACCESS_KEY_ID=...
+export AWS_SECRET_ACCESS_KEY=...
+export AWS_SESSION_TOKEN=...
+export $(aws-assume-role -role-arn arn:aws:iam::123456789:role/role/the-role)
+```
+
+Use instance roles:
+
+```sh
+export $(aws-assume-role -role-arn arn:aws:iam::123456789:role/role/the-role)
+```
+
+Validate and test the assumed role by running a command like `aws sts get-caller-identity`:
+
+```sh
+aws sts get-caller-identity
+{
+    "UserId": "AROAYU3CAIBXDSONTDJIN:aws-assume-role",
+    "Account": "123456789",
+    "Arn": "arn:aws:sts::123456789:assumed-role/the-role/aws-assume-role"
+}
+```
---
+++ cmd/aws-assume-role/main.go
@@ -0,0 +1,85 @@
+package main
+
+import (
+	"context"
+	"flag"
+	"fmt"
+	"github.com/aws/aws-sdk-go-v2/aws"
+	"github.com/aws/aws-sdk-go-v2/config"
+	"github.com/aws/aws-sdk-go-v2/credentials/stscreds"
+	"github.com/aws/aws-sdk-go-v2/service/sts"
+	"log"
+)
+
+const (
+	defaultRoleSessionName = "aws-assume-role"
+	version                = "0.0.1"
+)
+
+var (
+	roleARNArg, externalIDArg, roleSessionNameArg string
+	verboseArg, versionArg                        bool
+)
+
+func init() {
+	flag.StringVar(&roleARNArg, "role-arn", "", "The ARN of the role to assume")
+	flag.StringVar(&externalIDArg, "external-id", "", "An external ID to use when assuming the role")
+	flag.StringVar(&roleSessionNameArg, "role-session-name", defaultRoleSessionName, "An identifier for the assumed role session (A default is used if not supplied)")
+
+	flag.BoolVar(&verboseArg, "verbose", false, "Enable verbose output")
+	flag.BoolVar(&versionArg, "version", false, "Print version")
+
+	flag.Parse()
+}
+
+func assumeRole() *aws.Credentials {
+	ctx := context.TODO()
+	cfg, err := config.LoadDefaultConfig(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	client := sts.NewFromConfig(cfg)
+	id, err := client.GetCallerIdentity(ctx, &sts.GetCallerIdentityInput{})
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	if verboseArg {
+		log.Printf("initial auth arn: %s\n", aws.ToString(id.Arn))
+	}
+
+	stsClient := sts.NewFromConfig(cfg)
+	provider := stscreds.NewAssumeRoleProvider(stsClient, roleARNArg, func(o *stscreds.AssumeRoleOptions) {
+		o.RoleARN = roleARNArg
+		o.RoleSessionName = roleSessionNameArg
+		if externalIDArg != "" {
+			o.ExternalID = &externalIDArg
+		}
+	})
+
+	cfg.Credentials = aws.NewCredentialsCache(provider)
+	creds, err := cfg.Credentials.Retrieve(ctx)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return &creds
+}
+
+func printVersion() {
+	fmt.Println(version)
+}
+
+func printEnvCredentials(credentials aws.Credentials) {
+	fmt.Printf("AWS_ACCESS_KEY_ID=%s AWS_SECRET_ACCESS_KEY=%s AWS_SESSION_TOKEN=%s", credentials.AccessKeyID, credentials.SecretAccessKey, credentials.SessionToken)
+}
+
+func main() {
+	if versionArg {
+		printVersion()
+		return
+	}
+	creds := assumeRole()
+	printEnvCredentials(*creds)
+}
